d9143cceba976a1f44532ad6e9ad751556840342
Yang Taehwan <taehwan920@gmail.com>
2020-04-14 16:38:07 +0900

    How to CRUD with express

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
+var qs = require('querystring');
 
 const app = express();
 
@@ -32,7 +33,7 @@ app.get('/page/:pageId', (req, res) => {
       var html = template.HTML(sanitizedTitle, list,
         `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
         ` <a href="/create">create</a>
-                    <a href="/update?id=${sanitizedTitle}">update</a>
+                    <a href="/update/${sanitizedTitle}">update</a>
                     <form action="delete_process" method="post">
                       <input type="hidden" name="id" value="${sanitizedTitle}">
                       <input type="submit" value="delete">
@@ -42,6 +43,99 @@ app.get('/page/:pageId', (req, res) => {
     });
   });
 });
+
+app.get('/create', (req, res) => {
+  fs.readdir('./data', function (error, filelist) {
+    var title = 'WEB - create';
+    var list = template.list(filelist);
+    var html = template.HTML(title, list, `
+              <form action="/create_process" method="post">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p>
+                  <textarea name="description" placeholder="description"></textarea>
+                </p>
+                <p>
+                  <input type="submit">
+                </p>
+              </form>
+            `, '');
+    res.send(html);
+  });
+})
+
+app.post('/create_process', (req, res) => {
+  var body = '';
+  req.on('data', function (data) {
+    body = body + data;
+  });
+  req.on('end', function () {
+    var post = qs.parse(body);
+    var title = post.title;
+    var description = post.description;
+    fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+      res.redirect('/')
+    })
+  });
+})
+
+app.get('/update/:pageId', (req, res) => {
+  fs.readdir('./data', function (error, filelist) {
+    var filteredId = path.parse(req.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
+      var title = req.params.pageId;
+      var list = template.list(filelist);
+      var html = template.HTML(title, list,
+        `
+        <form action="/update_process" method="post">
+          <input type="hidden" name="id" value="${title}">
+          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+          <p>
+            <textarea name="description" placeholder="description">${description}</textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+        `,
+        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+      );
+      res.send(html);
+    });
+  });
+});
+
+app.post('/update_process', (req, res) => {
+  var body = '';
+  req.on('data', function (data) {
+    body = body + data;
+  });
+  req.on('end', function () {
+    var post = qs.parse(body);
+    var id = post.id;
+    var title = post.title;
+    var description = post.description;
+    fs.rename(`data/${id}`, `data/${title}`, function (error) {
+      fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+        res.redirect(`/page/${title}`)
+      })
+    });
+  });
+});
+
+app.post('/page/delete_process', (req, res) => {
+  var body = '';
+  req.on('data', function (data) {
+    body = body + data;
+  });
+  req.on('end', function () {
+    var post = qs.parse(body);
+    var id = post.id;
+    var filteredId = path.parse(id).base;
+    fs.unlink(`data/${filteredId}`, function (error) {
+      res.redirect('/');
+    })
+  });
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
